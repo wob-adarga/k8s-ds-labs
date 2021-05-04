3f11cb49b19093cbb1af7f2ed2762b266eae4eed
Olly Stephens <olly@adarga.ai>
2021-05-04 15:58:23 +0100

    chore: removing ds-lab-2

--- environments/ds-labs/ds-lab-2/platform/kubeflow/remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export AWS_PROFILE=adarga-datascience-admin
-export AWS_DEFAULT_REGION=eu-west-2
-export AWS_SDK_LOAD_CONFIG=1
-
-kfctl delete -V -f ./kfctl_aws_cognito.v1.2.0.yaml --delete_storage --force-deletion
